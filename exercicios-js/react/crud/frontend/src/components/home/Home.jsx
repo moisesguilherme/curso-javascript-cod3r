5564e17db8b50f683414597558e25fb5bbf8f61d
Moisés Guilherme <moisescomp@gmail.com>
2020-05-02 21:11:12 -0300

    React

---
+++ exercicios-js/react/crud/frontend/src/components/home/Home.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import Main from '../template/Main'
+
+export default props =>
+    <Main icon="home" title="Início"
+        subtitle="Segundo Projeto do capítulo de React.">
+        <div className='display-4'>Bem Vindo!</div>
+        <hr />
+        <p className="nb-0">Sistema para exemplificar a construçã
+        de um cadastro desenvolvido em React!</p>
+    </Main>
---
+++ exercicios-js/react/crud/frontend/src/components/template/Nav.jsx
@@ -0,0 +1,16 @@
+import './Nav.css'
+import React from 'react'
+
+export default props =>
+ <aside className="menu-area">
+    <nav className="menu">
+        {/* Refatorar em casa! */}
+        <a href="#/">
+            <i className="fa fa-home"></i> Início
+        </a>
+        <a href="#/users">
+            <i className="fa fa-users"></i> Usuários
+        </a>
+
+    </nav>
+ </aside>
