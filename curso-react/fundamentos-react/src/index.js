6f54fc361d8b2439478415b0c59c83f153082547
Moisés Guilherme <moisescomp@gmail.com>
2020-05-09 14:43:48 -0300

    Propriedade são somente leitura

--- curso-react/fundamentos-react/src/index.js
+++ curso-react/fundamentos-react/src/index.js
@@ -3,10 +3,15 @@ import ReactDOM from 'react-dom'
 import React from 'react'
 
 import Primeiro from './components/basicos/Primeiro'
+import ComParametro from './components/basicos/ComParametro'
 
 ReactDOM.render(
-    <div>
+    <div id="app">
         <Primeiro></Primeiro>
+        <ComParametro 
+            titulo="Situação do Aluno" 
+            aluno="Pedro Silva" 
+            nota={9.3} />
     </div>,
     document.getElementById('root')
  )
