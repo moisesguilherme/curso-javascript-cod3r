07529b86ccaa20f7e640681b3571bdb670f2c280
Moisés Guilherme <moisescomp@gmail.com>
2020-06-20 21:14:33 -0300

    Uso de Hooks

--- curso-react/fundamentos-react/src/App.jsx
+++ curso-react/fundamentos-react/src/App.jsx
@@ -3,6 +3,7 @@ import React from 'react'
 
 import Card from './components/layout/Card'
 
+import IndiretaPai from './components/comunicacao/IndiretaPai'
 import DiretaPai from "./components/comunicacao/DiretaPai";
 import UsuarioInfo from "./components/condicional/UsuarioInfo"
 import ParOuImpar from "./components/condicional/ParOuImpar"
@@ -29,10 +30,13 @@ export default () =>
 
         <div className="Cards">
 
-        <Card titulo="#09 - Comunicação Direta" color="#59323C">
-            <DiretaPai></DiretaPai>               
-         </Card>
+            <Card titulo="#10 - Comunicação Indireta" color="#8BAD39">
+                <IndiretaPai></IndiretaPai>               
+            </Card>
 
+            <Card titulo="#09 - Comunicação Direta" color="#59323C">
+                <DiretaPai></DiretaPai>               
+            </Card>
 
             <Card titulo="#08 - Renderização Condicional" color="#982395">
                 <ParOuImpar numero={21}></ParOuImpar>
