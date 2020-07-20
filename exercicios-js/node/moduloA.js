0641001d9efb45b1d3348ad937c5dc9efe8ee139
Moisés Guilherme <moisescomp@gmail.com>
2020-07-20 20:12:02 -0300

    Sistema de módulos

---
+++ exercicios-js/node/moduloA.js
@@ -0,0 +1,10 @@
+// um arquivo representa um módulo
+// exportar algo e importar algo
+// commum js ???
+
+//Formas de exportar 
+console.log(this) //{} objeto vazio
+this.ola = 'Fala Pessoal'
+exports.bemVindo = 'Bem vindo ao node!'
+module.exports.ateLogo = 'Até próximo exemplo'
+
---
+++ exercicios-js/node/moduloB.js
@@ -0,0 +1,9 @@
+    let a = 2
+
+    module.exports = {
+        teste: a,
+        bomDia: 'Bom dia',
+        boaNoite() {
+            return 'Boa noite'
+        }
+    }
