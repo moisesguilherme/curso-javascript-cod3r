cc3f9b22629f02b4db231a3d3418b539effa9f0e
Moisés Guilherme <moisescomp@gmail.com>
2020-07-27 20:14:46 -0300

    pegar menor salário

---
+++ exercicios-js/node/funcionarios/funcionarios.js
@@ -0,0 +1,46 @@
+const url = 'http://files.cod3r.com.br/curso-js/funcionarios.json'
+const axios = require('axios')
+
+const chineses = f => f.pais === 'China'
+const mulheres = f => f.genero === 'F'
+const menorSalario = (func, funcAtual) => {
+    return func.salario < funcAtual.salario ? func : funcAtual   
+}
+
+axios.get(url).then(response => {
+    const funcionarios = response.data
+
+    const resultado = funcionarios
+        .filter(chineses)
+        .filter(mulheres)
+        .reduce(menorSalario)
+    //console.log(funcionarios.filter(pais).filter(mulher))
+    console.log(resultado)
+})
+
+
+
+
+
+
+    //const pais = funcionario => funcionario.pais === 'China'
+    //const mulher = funcionario => funcionario.genero === 'F'
+    
+    /*
+    let funcionarioComMenorSalario = null
+
+    const menorSalario = (funcionario, index, arr) => {
+        
+        if( funcionarioComMenorSalario === null ) funcionarioComMenorSalario = arr[0];
+
+        if(funcionario.salario < funcionarioComMenorSalario.salario ){
+            
+            funcionarioComMenorSalario = funcionario;
+        }
+    }
+*/
+
+
+
+
+
